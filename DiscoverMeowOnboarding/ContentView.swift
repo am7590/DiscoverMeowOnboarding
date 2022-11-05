0954650d7a1746eee236f925fd565aede5fc7569
Alek Michelson <am7590@rit.edu>
2022-11-05 15:25:44 -0400

    Dummy data + TabView

--- DiscoverMeowOnboarding/ContentView.swift
+++ DiscoverMeowOnboarding/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var pageIndex = 0
+    private let pages: [Page] = DummyData.onboardingViews
+    private let dotAppearance = UIPageControl.appearance()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView(selection: $pageIndex) {
+            ForEach(pages) { page in
+                VStack {
+                    Spacer()
+                    PageView(page: page)
+                    Spacer()
+                    OnboardingArrowView(arrow: page.arrowConfig)
+                        .onTapGesture {
+                            incrementPage()
+                        }
+                    Spacer()
+                }
+                .tag(page.tag)
+            }
         }
-        .padding()
+        .animation(.easeInOut, value: pageIndex)
+        .indexViewStyle(.page(backgroundDisplayMode: .interactive))
+        .tabViewStyle(PageTabViewStyle())
+        .onAppear {
+            dotAppearance.currentPageIndicatorTintColor = .black
+            dotAppearance.pageIndicatorTintColor = .gray
+        }
+    }
+    
+    func incrementPage() {
+        pageIndex += 1
     }
 }
 
---
+++ DiscoverMeowOnboarding/DummyData.swift
@@ -0,0 +1,17 @@
+//
+//  DummyData.swift
+//  DiscoverMeowOnboarding
+//
+//  Created by Alek Michelson on 11/5/22.
+//
+
+import SwiftUI
+
+public struct DummyData {
+    
+    static var onboardingViews: [Page] = [
+        Page.welcome,
+        Page.connectToSnap,
+        Page.verification
+    ]
+}
