b1c27763a92f00e6f888ca21c868f780c570e1c3
Alek Michelson <am7590@rit.edu>
2022-11-05 14:18:51 -0400

    Initial Commit

---
+++ DiscoverMeowOnboarding/DiscoverMeowOnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscoverMeowOnboardingApp.swift
+//  DiscoverMeowOnboarding
+//
+//  Created by Alek Michelson on 11/5/22.
+//
+
+import SwiftUI
+
+@main
+struct DiscoverMeowOnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
